589e440e5a1425c04a356cab9f3ae28238746b34
Matt Ferguson <mdfergus@gmail.com>
2018-06-27 18:58:40 -0400

    feature(routes) added get route

--- routes.js
+++ routes.js
@@ -1,1 +1,16 @@
+const request = require("request");
 
+const options = {
+  url: "https://ourAppHere/api/startAuth",
+  method: "GET",
+  headers: {
+    clientKey: "1234567890---123456789--asdfghjk367"
+  }
+};
+
+request(options, function(err, response, body) {
+  console.log("error:", err);
+  console.log("statusCode:", response && response.statusCode);
+  let json = JSON.parse(body);
+  console.log(json);
+});
